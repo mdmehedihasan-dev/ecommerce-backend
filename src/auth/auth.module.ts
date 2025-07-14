7163748749a344a8917e1e28114372ea33aad89e
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2025-07-13 21:12:18 -0600

    controller issues

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/users/user.schema';
+import { UserSchema } from 'src/users/user.schema';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entity/user.entity';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    // MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    TypeOrmModule.forFeature([User]),
     JwtModule.register({
       secret: 'my_jwt_secret',
       signOptions: { expiresIn: '1h' },
